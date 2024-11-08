ebf084f5a2209fb9d742b4a9f9fb27ffca390430
Roche Aubin <aroche@if501-219-16.insa-lyon.fr>
2024-11-08 09:12:03 +0100

    affichage plateau

--- plateau.c
+++ plateau.c
@@ -4,20 +4,38 @@
 #include <string.h>
 
 struct Board {
-    int * plateau[12];
-}
+    int plateau[12];
+};
 
-
-
-void afficher_plateau(Board * board) {
-    printf("Plateau actuel : {")
-    for (elem : board->plateau){
-        printf("%d | ", elem)
+void afficher_plateau(struct Board *board) {
+    printf("Plateau actuel : \n");
+    for (int i = 0; i < 12; i++) {
+        if (i != 11 && i != 5){
+            printf("%d (%d)| ", board->plateau[i], i+1);
+        }
+        else {
+            printf("%d (%d)", board->plateau[i], i+1);
+        }
+        if (i == 5){
+            printf("\n");
+        }
     }
-    printf("}")
+    printf("\n");
 }
 
-int main(int argc, char **argv){
-    Board *board = new Board();
-    afficher_plateau(&board);
+int main(int argc, char **argv) {
+    struct Board *board = (struct Board *)malloc(sizeof(struct Board));
+    if (board == NULL) {
+        printf("Erreur d'allocation mémoire");
+    }
+
+    for (int i = 0; i < 12; i++) {
+        board->plateau[i] = 0;
+    }
+
+    afficher_plateau(board);
+
+    free(board);
+
+    return 0;
 }
